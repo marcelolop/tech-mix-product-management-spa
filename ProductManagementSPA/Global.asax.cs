42ba44d484944d9c7aa406822c316d47a97bbce4
marcelolop <marcelolopfern@hotmail.com>
2024-03-10 17:29:00 -0500

    Updated product view, added currency format

--- ProductManagementSPA/Global.asax.cs
+++ ProductManagementSPA/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Ignore Spelling: Mvc
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
--- ProductManagementSPA/Startup.cs
+++ ProductManagementSPA/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Owin;
+﻿// Ignore Spelling: app
+
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(ProductManagementSPA.Startup))]
