9a4206a364c3061417faed259d3de176b14ceba1
marcelolop <marcelolopfern@hotmail.com>
2024-03-13 22:09:57 -0500

    Some improvements, added a delete modal

--- IPDAL/Product.cs
+++ IPDAL/Product.cs
@@ -11,19 +11,27 @@ namespace IPDAL
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     /// <summary>
     /// The Product class 
     /// </summary>
     public partial class Product
     {
         //Properties
+        [Required]
         public int ProductID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public decimal Price { get; set; }
+        [Required]
         public int Quantity { get; set; }
+        [Required]
         public string Category { get; set; }
+        [Required]
         public string Supplier { get; set; }
     }
 }
